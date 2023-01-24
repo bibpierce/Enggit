de2554d46834f77788cb9e107e8b006fddfbc52c
ef-Pierce <jsbibpierce@itcom888.com>
2023-01-24 15:45:04 +0800

    init

---
+++ src/main/java/com/co/enggit/EnggitApplication.java
@@ -0,0 +1,13 @@
+package com.co.enggit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnggitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnggitApplication.class, args);
+    }
+
+}
